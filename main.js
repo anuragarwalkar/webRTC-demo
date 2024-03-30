0e3f28f4710b204caae6ab836c31bac417706ec9
Anurag Arwalkar <anurag.arwalkar@globant.com>
2024-03-30 17:10:37 +0530

    added

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-
-let peerConnection = new RTCPeerConnection()
+let peerConnection = new RTCPeerConnection();
 let localStream;
 let remoteStream;
 
 let init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
-    remoteStream = new MediaStream()
-    document.getElementById('user-1').srcObject = localStream
-    document.getElementById('user-2').srcObject = remoteStream
+    localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+    remoteStream = new MediaStream();
+    document.getElementById('user-1').srcObject = localStream;
+    document.getElementById('user-2').srcObject = remoteStream;
 
     localStream.getTracks().forEach((track) => {
         peerConnection.addTrack(track, localStream);
@@ -15,54 +14,63 @@ let init = async () => {
 
     peerConnection.ontrack = (event) => {
         event.streams[0].getTracks().forEach((track) => {
-        remoteStream.addTrack(track);
+            remoteStream.addTrack(track);
         });
     };
-}
+
+    peerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+            // Append ICE candidate to the existing list of ICE candidates
+            document.getElementById('ice-candidates').value += JSON.stringify(event.candidate) + '\n';
+            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription);
+
+        }
+    };
+};
 
 let createOffer = async () => {
-
-
-    peerConnection.onicecandidate = async (event) => {
-        //Event that fires off when a new offer ICE candidate is created
-        if(event.candidate){
-            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
-        }
-    };
-
     const offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
-}
+};
 
 let createAnswer = async () => {
-
-    let offer = JSON.parse(document.getElementById('offer-sdp').value)
-
-    peerConnection.onicecandidate = async (event) => {
-        //Event that fires off when a new answer ICE candidate is created
-        if(event.candidate){
-            console.log('Adding answer candidate...:', event.candidate)
-            document.getElementById('answer-sdp').value = JSON.stringify(peerConnection.localDescription)
-        }
-    };
+    let offer = JSON.parse(document.getElementById('offer-sdp').value);
 
     await peerConnection.setRemoteDescription(offer);
-
     let answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer); 
-}
+    await peerConnection.setLocalDescription(answer);
+    
+    // Display SDP answer to be copied and sent to the other peer
+    document.getElementById('answer-sdp').value = JSON.stringify(peerConnection.localDescription);
+};
 
 let addAnswer = async () => {
-    console.log('Add answer triggerd')
-    let answer = JSON.parse(document.getElementById('answer-sdp').value)
-    console.log('answer:', answer)
-    if (!peerConnection.currentRemoteDescription){
-        peerConnection.setRemoteDescription(answer);
+    let answer = JSON.parse(document.getElementById('answer-sdp').value);
+    
+    if (!peerConnection.currentRemoteDescription) {
+        await peerConnection.setRemoteDescription(answer);
     }
-}
+};
 
-init()
+let addIceCandidates = () => {
+    let candidates = document.getElementById('ice-candidates').value.split('\n');
+    candidates.forEach(candidate => {
+        if (candidate.trim() !== '') {
+            let iceCandidate = JSON.parse(candidate);
+            peerConnection.addIceCandidate(new RTCIceCandidate(iceCandidate))
+                .then(() => {
+                    console.log('Ice candidate added successfully');
+                })
+                .catch((error) => {
+                    console.error('Error adding ice candidate:', error);
+                });
+        }
+    });
+};
 
-document.getElementById('create-offer').addEventListener('click', createOffer)
-document.getElementById('create-answer').addEventListener('click', createAnswer)
-document.getElementById('add-answer').addEventListener('click', addAnswer)
+init();
+
+document.getElementById('create-offer').addEventListener('click', createOffer);
+document.getElementById('create-answer').addEventListener('click', createAnswer);
+document.getElementById('add-answer').addEventListener('click', addAnswer);
+document.getElementById('add-ice-candidates').addEventListener('click', addIceCandidates);
